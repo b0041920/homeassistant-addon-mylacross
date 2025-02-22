45e5641853757603904444832aeabe2411493514
b0041920 <b0041920@web.de>
2025-02-22 08:14:06 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Eventuell kannst du hier noch Umgebungsvariablen setzen oder Log-Ausgaben konfigurieren
+
+# Starte mylacross (ggf. mit gewünschten Parametern)
+exec mylacross
