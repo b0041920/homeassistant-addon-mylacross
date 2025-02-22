8a5aae4a9111b07380509e342d05aa7a437d3f38
b0041920 <b0041920@web.de>
2025-02-22 09:01:09 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Eventuell kannst du hier noch Umgebungsvariablen setzen oder Log-Ausgaben konfigurieren
-
-# Starte mylacross (ggf. mit gewünschten Parametern)
-exec mylacross
